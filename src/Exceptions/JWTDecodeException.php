c2639c17a177854dca7d5f50c7c2cfc266e07571
ulasoft <ulasoft@mail.ru>
2022-03-31 12:19:46 +0500

    bugs fixed

---
+++ src/Exceptions/JWTDecodeException.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Usoft\Auth\Exceptions;
+
+class JWTDecodeException extends \Exception
+{
+
+}
---
+++ src/Providers/AuthProvider.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Usoft\Auth\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class AuthProvider extends ServiceProvider
+{
+
+    public function boot()
+    {
+    }
+
+    public function register()
+    {
+    }
+}
